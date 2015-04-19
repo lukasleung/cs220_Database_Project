fe9f28b1364f4b7c53d2ca74430b79a577bb17ff
Niwamoto <Yakisoba@Nicholass-MacBook-Air.local>
2015-04-19 16:12:57 -0400

    Finished SQL Tables Draft
    Valid syntax that is accepted by MYSQL

--- CreateSqlTables.sql
+++ CreateSqlTables.sql
@@ -1,27 +1,78 @@
-CREATE TABLE user(
-	uid INT AUTO_INCREMENT PRIMARY KEY,
-	first_name varchar(255),
-	last_name varchar (255),
-	points INT DEFAULT 0,
-	email varchar(20) NOT NULL UNIQUE,
-	username varchar(20) NOT NULL UNIQUE,
-	password varchar (20) NOT NULL
+-- cid comment id
+-- pid post id
+-- uid user id
+-- pos like
+
+CREATE TABLE region(
+	rid INT AUTO_INCREMENT,
+	location varchar(20) NOT NULL,
+	PRIMARY KEY (rid),
+	UNIQUE(location)
 )
 
-CREATE TABLE post
-	pid INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE users(
+	uid INT AUTO_INCREMENT,
+	rid INT NOT NULL,
+	pos INT DEFAULT 0,
+	neg INT DEFAULT 0,
+	PRIMARY KEY (uid),
+	FOREIGN KEY (rid) REFERENCES region(rid)
+)
+
+CREATE TABLE posts(
+	pid INT AUTO_INCREMENT,
 	uid INT NOT NULL,
-	content varchar (255) NOT NULL
-	time timestamp DEFAULT CURRENT_TIMESTAMP
-	
+	content varchar (140) NOT NULL,
+	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	rid INT NOT NULL,
+-- 	varchar(20) NOT NULL, 
+	comments INT DEFAULT 0,
+	pos INT DEFAULT 0,
+	neg INT DEFAULT 0,
+	PRIMARY KEY (pid),
+	FOREIGN KEY (rid) REFERENCES region(rid)
+)
+-- takes user region
+
+
+CREATE TABLE comments(
+	cid INT AUTO_INCREMENT,
+	pid INT NOT NULL,
+	uid INT NOT NULL,
+	content varchar (140) NOT NULL,
+	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	pos INT DEFAULT 0,
+	neg INT DEFAULT 0,
+	PRIMARY KEY (cid)
+-- 	PRIMARY KEY (pid, cid)
+)
+-- CID counts the number of comments on a post
+
+CREATE TABLE posts_likes(
+	uid INT NOT NULL,
+	pid INT NOT NULL,
+	pos BOOLEAN NOT NULL,
+	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (uid, pid),
+	FOREIGN KEY (pid) REFERENCES posts(pid),
+	FOREIGN KEY (uid) REFERENCES users(uid)
 )
 
--- CREATE TABLE comment(
--- 	id INT AUTO_INCREMENT PRIMARY KEY,
--- 	first_name varchar(255)
--- 	last_name varchar (255)
--- 	points INT DEFAULT 0
--- 	email varchar(20) NOT NULL UNIQUE
--- 	username varchar(20) NOT NULL UNIQUE
--- 	password varchar (20) NOT NULL
--- )
+CREATE TABLE comments_likes(
+	uid INT NOT NULL,
+	pid INT NOT NULL,
+	pos BOOLEAN NOT NULL,
+	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (uid, pid),
+	FOREIGN KEY (pid) REFERENCES posts(pid),
+	FOREIGN KEY (uid) REFERENCES users(uid)
+)
+
+
+-- choose view by region
+
+-- organize by most recent 
+-- organize by most pos
+-- organize by most neg
+-- organize by oldest time
+-- organize by most commentsp
