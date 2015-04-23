1764b8cdf9b2c532e43d70aa5c84434970bb7e8a
Niwamoto <Yakisoba@Nicholass-MacBook-Air.local>
2015-04-23 13:43:09 -0400

    Fixed Triggers for easy import
    Small fixes so that the files can be imported as is rather than copy
    paste

--- Triggers.sql
+++ Triggers.sql
@@ -1,13 +1,14 @@
 -- -------------------------------------------------------------------------(Add a Comment)
 
 CREATE TRIGGER comment_add AFTER INSERT ON comments
+FOR EACH ROW
 UPDATE posts
 SET posts.comments = posts.comments + 1
 WHERE posts.pid = NEW.pid;
 
 -- -------------------------------------------------------------------------( (Dis)Like a Comment)
-
-CREATE TRIGGER `comment_feedback` AFTER INSERT ON `comments_likes`
+delimiter //
+CREATE TRIGGER comment_feedback AFTER INSERT ON comments_likes
 FOR EACH ROW 
 IF NEW.pos = 1 THEN
 	UPDATE users
@@ -17,14 +18,15 @@ ELSE
 	UPDATE users
 	SET users.neg = users.neg + 1
 	WHERE users.uid = NEW.ouid;
-END IF;
+END IF;//
+delimiter ;
 
 
 -- -------------------------------------------------------------------------( (Dis)Like a Post)
 
-CREATE TRIGGER 'post_feedback' AFTER INSERT ON 'posts_likes'
-FOR EACH ROW
-IF NEW.pos = 1 THEN
+delimiter //
+CREATE TRIGGER `post_feedback` AFTER INSERT ON `posts_likes`
+ FOR EACH ROW IF NEW.pos = 1 THEN
 	UPDATE users
 	SET users.pos = users.pos + 1
 	WHERE users.uid = NEW.ouid;
@@ -32,4 +34,5 @@ ELSE
 	UPDATE users
 	SET users.neg = users.neg + 1
 	WHERE users.uid = NEW.ouid;
-END IF;
+END IF;//
+delimiter ;
