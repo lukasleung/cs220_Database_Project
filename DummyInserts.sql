234b301f55e8d2fe3c062c6ce08f777cf0674578
Niwamoto <Yakisoba@Nicholass-MacBook-Air.local>
2015-04-24 14:13:16 -0400

    Added more Dummy input
    Shows the least amount of input needed for a sql insert query

--- DummyInserts.sql
+++ DummyInserts.sql
@@ -11,7 +11,15 @@ VALUES
 	(3, 'Coffee'),
 	(1, 'Food');
 
-
+-- uid, pos and neg are optional and have default values
+-- INSERT INTO users (rid)
+-- VALUES
+-- 	(1),
+-- 	(1),
+-- 	(2),
+-- 	(2),
+-- 	(3),
+-- 	(3);
 INSERT INTO users (uid, rid, pos, neg)
 VALUES
 	(1, 1, 0, 0),
@@ -21,25 +29,44 @@ VALUES
 	(5, 3, 0, 0),
 	(6, 3, 0, 0);
 
+-- pid, ts, comments, pos and neg are optional and have default values
+-- INSERT INTO posts (uid, content, rid)
+-- VALUES
+-- 	(1, 'You eated my cookies', 1),
+-- 	(2, 'Caffine is my hero', 2);
 INSERT INTO posts (pid, uid, content, ts, rid, comments, pos, neg)
 VALUES
 	(1, 1, 'You eated my cookies', '2015-04-23 14:03:12', 1, 0,0,0),
 	(2, 2, 'Caffine is my hero', '2015-04-23 14:03:49', 2, 0,0,0);
 
-
+-- ts is optional and has a default value
+-- INSERT INTO posts_likes (uid, pid, pos, ouid)
+-- VALUES
+-- 	(1, 1, 1, 1),
+-- 	(2, 1, 0, 1),
+-- 	(4, 2, 1, 3);
 INSERT INTO posts_likes (uid, pid, pos, ouid, ts)
 VALUES
 	(1, 1, 1, 1, '2015-04-23 14:04:53'),
 	(2, 1, 0, 1, '2015-04-23 14:05:21'),
 	(4, 2, 1, 3, '2015-04-23 14:07:25');
 
-
+-- cid, ts, pos and neg are optional and have default values
+-- INSERT INTO comments (pid, uid, content)
+-- VALUES
+-- 	(1, 2, 'Those were my cookies.'),
+-- 	(2, 4, 'My house smells like coffee');
 INSERT INTO comments (cid, pid, uid, content, ts, pos, neg)
 VALUES
 	(1, 1, 2, 'Those were my cookies.', '2015-04-23 14:08:25',0,0),
 	(2, 2, 4, 'My house smells like coffee', '2015-04-23 14:09:11',0,0);
 
-
+-- ts is optional and has a default value
+-- INSERT INTO comments_likes (cid, uid, pos, ouid)
+-- VALUES
+-- 	(1, 2, 1, 1),
+-- 	(2, 3, 1, 4);
+ 
 INSERT INTO comments_likes (cid, uid, pos, ouid, ts)
 VALUES
 	(1, 2, 1, 1, '2015-04-23 14:09:44'),
