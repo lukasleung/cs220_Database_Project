f5598e26e516841fdd8378274fefbe4ae19a4f9c
aznator <daoxle@yahoo.com>
2015-05-01 11:26:52 -0400

    added another sort bar

--- TrishPlayRoom/PrintBarsViews.py
+++ TrishPlayRoom/PrintBarsViews.py
@@ -31,6 +31,42 @@ def printSortTopBar(uid,rid,pgid):
         <div class="col1">
 		"""
 
+
+def printNewRegionSortBar(uid,rid,pgid,nrid):
+
+	print """
+	 <!DOCTYPE html>
+        	<html>
+        		<head>
+        			<title>Bison Unlimited</title>
+        			<link rel="stylesheet" type="text/css" href="css/stylesheet.css" media="screen" />
+        		</head>
+        				<body>
+        					<div id="header">
+        					<img src="css/bison.jpg" height=30px width=50px style="float:left;">
+        					<h3> Bison...Say Something
+        						<FORM type=post action=BisonController.py style="float:right;padding-right:12%;">
+        							<select name="order_by">
+        								<option value="recent">Most Recent</option>
+        								<option value="positive">Most Positive</option>
+       									<option value="negative">Most Negative</option>
+        								<option value="controversial">Most Controversial</option>
+        								<option value="comments">Most Comments</option>
+        								<option value="interaction">Most Interaction</option>
+       				 				</select>
+       				 					<input type=submit name=sort> 
+        								<input type=hidden name=user_id value="""+ str(uid) +""">
+										<input type=hidden name=region_id value="""+ str(rid) +""">
+										<input type=hidden name=page_id value="""+ str(pgid) +""">
+										<input type=hidden name=chooseRegion="""+ str(nrid) +"""> 
+                				</FORM>
+                			</div>
+                
+                <div class="colmask leftmenu">
+            <div class="colleft">
+        <div class="col1">
+		"""
+	
 def printPlainTopBar():
     print """
         <!DOCTYPE html>
@@ -95,9 +131,9 @@ def printAllPosts(uid, rid, pgid):
             				<FORM METHOD=POST action=BisonController.py>
             					Choose A Region
            					 		<select name=chooseRegion >
-            							<option value="1">Under Where?</option>
-            							<option value="3">Over There</option>
-            							<option value="2">Outside Your Window</option>
+            							<option value="1">West Coast?</option>
+            							<option value="3">East Coast</option>
+            							<option value="2">Bible Belt</option>
            		 					</select>
            		 					<input type=submit name=post> 
     								<input type=hidden name=user_id value="""+ str(uid) +""">
